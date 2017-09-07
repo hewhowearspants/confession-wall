0943d43a15b1142a9696ee49e79002d58430b217
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-07 18:15:16 -0400

    add controller methods for index, show, create

--- app/controllers/confessions_controller.rb
+++ app/controllers/confessions_controller.rb
@@ -1,2 +1,18 @@
 class ConfessionsController < ApplicationController
+  def index
+    @confessions = Confession.all
+    @categories = Category.all
+  end
+
+  def show
+    @confession = Confession.find(params[:id])
+  end
+
+  def create
+    confession = params['confession']
+    Confession.create!(text: confession['text'], category_id: confession['category_id'])
+    
+    redirect_to confessions_path
+  end
+
 end
