023daa62e87d4b939df12e9cde3695d2cc049144
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-07 17:29:20 -0400

    add routes for confessions, categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :confessions
+  resources :categories, only: [:index, :show]
 end
