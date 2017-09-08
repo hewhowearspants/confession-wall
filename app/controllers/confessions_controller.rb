a270da87dbba84b4a0a392903c6d9d40db906048
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-07 20:42:28 -0400

    add edit, destroy methods to confessions controller

--- app/controllers/confessions_controller.rb
+++ app/controllers/confessions_controller.rb
@@ -15,4 +15,22 @@ class ConfessionsController < ApplicationController
     redirect_to confessions_path
   end
 
+  def destroy
+    Confession.destroy(params[:id])
+
+    redirect_to confessions_path
+  end
+
+  def edit
+    @categories = Category.all
+    @confession = Confession.find(params[:id])
+  end
+
+  def update
+    confession = params['confession']
+    Confession.update(params[:id], text: confession['text'], category_id: confession['category_id'])
+  
+    redirect_to confession_path(params[:id])
+  end
+
 end
